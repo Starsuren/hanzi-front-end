01d764c6de7027b581c2d4de31ad9691eb11b60f
Floxiz <dipars@gmail.com>
2021-04-08 22:11:28 +0100

    added animation for search

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import {useEffect, useState} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
 
 const variants = {
-hidden:{ opacity:1, border:'1px dashed #999ca1',borderRadius:15},
 visible:{border:'3px solid #8b9fc4', borderRadius:10 , transition:{type:'spring', stiffness:40, delay:5}}
 }
 
@@ -31,7 +30,7 @@ const Home =  () =>  {
   }, [])
 
   const [state,setState] = useState(false);
-  
+ 
   const inputHandler = () => {
 
 
@@ -50,17 +49,28 @@ const Home =  () =>  {
 
 
       <main className={styles.main}>
-      <motion.div className={state ? [styles.main__box, styles['main__box--open']].join(' '): styles.main__box} variants={variants} initial="hidden" animate={state && 'visible'}>
-       <AnimatePresence >{!state && (<><motion.div exit={{opacity:0,y:-400, transition:{velocity:300, type:'tween'}}}initial="hidden" animate="visible" className={styles.main__intro}>
+      <AnimatePresence >
+        {!state &&  
+      <motion.div exit={{opacity:0,width:0,height:0,transition:{velocity:20, duration:0.4,delay:0.4,type:'tween'}}} className={styles.main__box}>
+      <motion.div exit={{opacity:0,transition:{ velocity:50, type:'tween'}}} className={styles.main__intro}>
           <h1>Hanzi: A comprehensive Chinese dictionary</h1>
         </motion.div>
-         
-      <motion.div exit={{opacity:0}} className={state? styles['search--box-open'] : styles.search}>
+      <motion.div  exit={{opacity:0,transition:{duration:0.2,type:'tween'}}} className={styles.search}>
       <input placeholder =" " className={styles.search__input} onChange={()=>setState(true)} type="search" id="search" name="search"/>
-      <label className={state? styles['search__label--box-open'] : styles.search__label} htmlFor="search">Search</label>
-      </motion.div></>) }
-      </AnimatePresence> 
-      {state && 
+      <label className={styles.search__label} htmlFor="search">Search</label>
+      </motion.div> 
+</motion.div>
+
+}
+</AnimatePresence>
+{state &&
+      <motion.div  className={[styles.main__box, styles['main__box--open']].join(' ')} variants={variants} initial="hidden" animate={state && 'visible'}>
+      <motion.div initial="" animate="" className={styles['search--box-open']}>
+      <input placeholder =" " className={styles.search__input} onChange={()=>setState(true)} type="search" id="search" name="search"/>
+      <label className={styles['search__label--box-open']} htmlFor="search">Search</label>
+      </motion.div> 
+     
+
       <div className={styles.main__box__results}> 
       <motion.div className={`${styles['main__box__results__item--char']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
       <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
@@ -75,15 +85,17 @@ const Home =  () =>  {
       <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
       <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
       </div>
+      </motion.div>
+
 }
 
-      </motion.div>
+
+
+
       </main>
     </div>
   )
 }
 
   export default withApollo({ ssr: true })(Home);
-
-
 
