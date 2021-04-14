172cd699b5ab82cbbcdc1fa65bd27aa74bdbf12d
Sun Jiefeng <dipars@gmail.com>
2021-04-14 14:41:48 +0100

    minor update again

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ import {motion, AnimatePresence} from 'framer-motion';
 import { valueFromAST } from 'graphql';
 
 const variants = {
-visible:{border:'2px solid #8b9fc4', borderRadius:10, opacity:1, transition:{delay:0.7, type:'tween'}},
-hidden:{border:'0px solid #8b9fc4', opacity:0}
+visible:{display:'block',border:'2px solid #8b9fc4', borderRadius:10, opacity:1, transition:{delay:0.8, type:'tween'}},
+hidden:{display:'none',border:'0px solid #8b9fc4', opacity:0}
 }
 
 
@@ -49,7 +49,7 @@ const Home =  () =>  {
       <main className={styles.main}>
       <AnimatePresence >
         {state===null &&  
-      <motion.div exit={{opacity:0,width:0,height:0,transition:{velocity:20, duration:0.4,delay:0.4,type:'tween'}}} className={styles.main__box}>
+      <motion.div exit={{opacity:0,width:0,height:0,transition:{velocity:20, duration:0.4,delay:0.3,type:'tween'}}} className={styles.main__box}>
       <motion.div exit={{opacity:0,transition:{ velocity:50, type:'tween'}}} className={styles.main__intro}>
           <h1>Hanzi: A comprehensive Chinese dictionary</h1>
         </motion.div>
@@ -61,10 +61,10 @@ const Home =  () =>  {
 
 }
 </AnimatePresence>
-{!state===null &&
+{typeof state === 'string' &&
       <motion.div  className={[styles.main__box,styles['main__box--open']].join(' ')} variants={variants} initial='hidden' animate='visible'>
       <motion.div initial="" animate="" className={styles['search--box-open']}>
-      <input placeholder =" " value={state!} className={styles.search__input} onChange={(e)=>setState(e.currentTarget.value)} type="search" id="search" name="search" autoFocus/>
+      <input placeholder =" " value={state} className={styles.search__input} onChange={(e)=>setState(e.currentTarget.value)} type="search" id="search" name="search" autoFocus  />
       <label className={styles['search__label--box-open']} htmlFor="search">Search</label>
       </motion.div> 
      
@@ -84,7 +84,6 @@ const Home =  () =>  {
       <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
       </div>
       </motion.div>
-
 }
 
 
