620a241402d4d15d446f4b2e16aac4b310702996
Sun Jiefeng <dipars@gmail.com>
2021-05-04 02:50:15 +0100

    index updated search

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,31 @@ import Spinner from '../components/spinner/Spinner';
 import {useEffect, useState, useRef} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
 import { valueFromAST } from 'graphql';
+import {useFindCharQuery} from '../generated/graphql';
+
 
 const variants = {
-visible:{display:'block',border:'2px solid #8b9fc4', borderRadius:10, opacity:1, transition:{delay:0.8, type:'tween'}},
-hidden:{display:'none',border:'0px solid #8b9fc4', opacity:0}
+visible:{ display:'block',y:0, borderRadius:10, opacity:1, transition:{duration:0.3,delay:0.5, type:'tween'}},
+hidden:{display:'none',y:500,border:'2px solid #8b9fc4',opacity:0}
 }
 
 
 const Home =  () =>  {
 
-
-   
-  
-  useEffect(() => {
-    const debounce = setTimeout(() => {
-    
-  }, 2000);
-     
-    return () => {
-      clearTimeout()
-    }
-  }, [])
-
+  const {data,loading} = useFindCharQuery({variables:{char:['z'],options:{characters:true,sentences:true,words:true}}})
+  const inputRef = useRef<HTMLInputElement|null>(null);
   const [state,setState] = useState<string|null>(null);
- 
+  useEffect(() => {
+    if (state) {
+     setTimeout(()=>inputRef.current!.focus(),1000) ;
+    }
+  }, [state])
+
+ const dataResults = data && data!.findChar.map((v)=><div className={`${styles[`main__box__results__item--${v.__typename}`]} ${styles.main__box__results__item}`}>{[v.char_detail.character,v.char_detail.meaning,v.char_detail.pinyin]}</div>)
+
+
+
   const inputHandler = () => {
-
-
   }
  
   return (
@@ -54,7 +52,7 @@ const Home =  () =>  {
           <h1>Hanzi: A comprehensive Chinese dictionary</h1>
         </motion.div>
       <motion.div  exit={{opacity:0,transition:{duration:0.2,type:'tween'}}} className={styles.search}>
-      <input placeholder =" " className={styles.search__input} onChange={(e)=>setState(e.currentTarget.value)} type="search" id="search" name="search"/>
+      <input placeholder =" " className={styles.search__input} onKeyPress={(e)=>{if(e.key==='Enter')setState(e.currentTarget.value)}} type="search" id="search" name="search"/>
       <label className={styles.search__label} htmlFor="search">Search</label>
       </motion.div> 
 </motion.div>
@@ -64,33 +62,16 @@ const Home =  () =>  {
 {typeof state === 'string' &&
       <motion.div  className={[styles.main__box,styles['main__box--open']].join(' ')} variants={variants} initial='hidden' animate='visible'>
       <motion.div initial="" animate="" className={styles['search--box-open']}>
-      <input placeholder =" " value={state} className={styles.search__input} onChange={(e)=>setState(e.currentTarget.value)} type="search" id="search" name="search" autoFocus  />
-      <label className={styles['search__label--box-open']} htmlFor="search">Search</label>
+      <input placeholder =" " value={state} className={[styles.search__input,styles.search__input__open].join(' ')} onChange={(e)=>setState(e.currentTarget.value)} type="search" id="search" name="search" ref={inputRef} autoFocus  />
+      <label className={styles['search__label--box-open']}  htmlFor="search">Search</label>
       </motion.div> 
-     
-
       <div className={styles.main__box__results}> 
-      <motion.div className={`${styles['main__box__results__item--char']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--char']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--char']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--char']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--word']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
-      <motion.div className={`${styles['main__box__results__item--sent']} ${styles.main__box__results__item}`}>中 meaning:Middle pinyin:Zhong </motion.div>
+      {dataResults ? dataResults : (<div><h1>No results were found</h1><p>Try a different term or remove filters</p></div>)}
       </div>
       </motion.div>
 }
-
-
-
-
-      </main>
-    </div>
+  </main>
+  </div>
   )
 }
 
