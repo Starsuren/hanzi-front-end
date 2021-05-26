066de06aa3a71db7fb0b352d7c2641fbe0fc8030
Floxiz <dipars@gmail.com>
2021-05-26 20:16:01 +0100

    modified modal to use a portal

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,10 +5,10 @@ import {Header} from './Header'
 export const Layout:React.FC<{}> = ({children}) => (  
   
 <div className="allContent">
+<div id="portal"></div>
 <Header>
 <Navbar/> 
 </Header>
-
 {children}
 <Footer/>
 </div>
--- components/UI/Modal.tsx
+++ components/UI/Modal.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import {useEffect,useState} from 'react';
 import {AnimatePresence,motion} from 'framer-motion';
 import classes from './Modal.module.scss';
+import {createPortal} from 'react-dom';
 
  
 const variants = {
@@ -10,7 +11,12 @@ visible:{opacity:1}
 }
 
 const Modal:React.FC<{showModal:boolean,setModal:any}> = ({showModal,setModal}) => {
- return <AnimatePresence> {showModal ? <motion.div onClick={setModal} exit={{opacity:0}}variants={variants} animate="visible" initial="hidden" className={classes.modal}></motion.div>:null} </AnimatePresence>
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+  setMounted(true);
+  }, [showModal])
+
+ return mounted ? createPortal(<AnimatePresence> {showModal ? <motion.div onClick={setModal} exit={{opacity:0}}variants={variants} animate="visible" initial="hidden" className={classes.modal}></motion.div>:null} </AnimatePresence>,document.getElementById('portal')!):null; 
   }
 
 export default Modal;
